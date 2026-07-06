832c7a62140ad9325bc75680e1358d9f6b78a958
devbylin <saulhstore@gmail.com>
2026-07-06 14:41:30 -0500

    listarImpares

---
+++ ejercicio.js
@@ -0,0 +1,32 @@
+
+function listarNumeros(){
+    for(let i= 0; i <= 3; i++){
+        console.log(i)
+    }
+}
+function listarNumerosReversa(){
+    for(let i= 3; i>0; i--){
+        console.log(i)
+    }
+}
+function listarPares(){
+    for(let i =0; i< 10; i+=2){
+        console.log(i)
+    }
+}
+function listarImpares(){
+    for(let i= 1; i< 8; i+=2){
+        console.log(i)
+    }
+}
+function ejecutar(numEjercicio){
+    if(numEjercicio == 1){
+        listarNumeros()
+    }else if(numEjercicio ==2){
+        listarNumerosReversa();
+    }else if(numEjercicio ==3){
+        listarPares();
+    }else if(numEjercicio ==4){
+        listarImpares();
+    }
+}
