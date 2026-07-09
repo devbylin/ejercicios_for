29066fea7d415d15dbd19d28bdb12991743a3d57
devbylin <saulhstore@gmail.com>
2026-07-08 19:56:17 -0500

    funcionalidad correcta para generar tablas 3 y 5

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,4 +1,36 @@
 function generarTablas(){
-    let contenedor = document.getElementById("contenedorTabla");
-    contenedor = contenedor.innerHTML = "<h1>PROBANDO<H1>";
+     let contenido = "";
+     let multiplicando = 5;
+     contenido = `
+        <table class="tablaMultiplicar">
+            <thead>
+                <tr>
+                    <th>Multiplicando</th>
+                    <th>×</th>
+                    <th>Multiplicador</th>
+                    <th>=</th>
+                    <th>Resultado</th>
+                </tr>
+            </thead>
+            <tbody>
+        `;
+        for(let i = 1; i <= 10; i++){
+            let resultado = multiplicando * i;
+            contenido += `
+            <tr>
+                <td>${multiplicando}</td>
+                <td>×</td>
+                <td>${i}</td>
+                <td>=</td>
+                <td>${resultado}</td>
+            </tr>
+        `;
+        }
+         contenido += `
+                </tbody>
+            </table>
+        `;
+
+     let contenedor = document.getElementById("contenedorTabla");
+    contenedor.innerHTML = contenido;
 }
